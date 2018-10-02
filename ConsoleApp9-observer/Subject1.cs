9599bc71b4136e6a4c6ae3b87797bc5d615e8001
TethyLolind <lolind@yeah.net>
2018-10-02 23:51:08 +0800

    test!

--- ConsoleApp9-observer/Subject1.cs
+++ ConsoleApp9-observer/Subject1.cs
@@ -4,49 +4,41 @@ namespace ConsoleApp9_observer
 {
     internal class Subject1
     {
-        private int parameter1;
-        private int parameter2;
-        public  List<IObserber1> oberserList;
+        private int _parameter1;
+        private int _parameter2;
+        public List<IObserber1> OberserList;
 
         public Subject1()
         {
-            this.oberserList=new List<IObserber1>();
-        }
-        public void setParameter1(int para1, int para2)
-        {
-            this.parameter1 = para1;
-            this.parameter2 = para2;
-            parameterchanged();
+            OberserList = new List<IObserber1>();
         }
 
-        private void parameterchanged()
+        public void setParameter1(int para1, int para2)
+        {
+            _parameter1 = para1;
+            _parameter2 = para2;
+            Parameterchanged();
+        }
+
+        private void Parameterchanged()
         {
             notifyObserver();
         }
 
         public void notifyObserver()
         {
-            foreach (var obserber in oberserList)
-            {
-                obserber.update(this.parameter1, this.parameter2);
-            }
+            foreach (var obserber in OberserList) obserber.update(_parameter1, _parameter2);
         }
-      
 
         public void register(Obserber1 obserber1)
         {
-            this.oberserList.Add(obserber1);
-            
+            OberserList.Add(obserber1);
         }
 
         public void remove(Obserber1 obserber1)
         {
-            var index=oberserList.IndexOf(obserber1);
-            if (index>0)
-            {
-                oberserList.Remove(obserber1);
-            }
+            var index = OberserList.IndexOf(obserber1);
+            if (index > 0) OberserList.Remove(obserber1);
         }
-
     }
 }
