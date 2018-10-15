3d0a2a2ae90cff81ddb00f67d1a7602ec8a8d855
Bo Zhang <BZhang26@slb.com>
2018-10-15 15:05:36 +0800

    加了avl的主函数

--- AVLTree/Program.cs
+++ AVLTree/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using _2Tree;
 
 namespace AVLTree
 {
@@ -14,9 +15,21 @@ namespace AVLTree
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            Random random = new Random();
+
+            TreeClass a = new TreeClass(new BinaryTreeNode<int>(50));
+            for (int i = 0; i < 100; i++)
+            {
+                a.AddTo(new BinaryTreeNode<int>(random.Next(1, 100)));
+            }
+            a.AddTo(new BinaryTreeNode<int>(199));
+
+            a.Remove(new BinaryTreeNode<int>(50));
+            Console.WriteLine(a.Contains(new BinaryTreeNode<int>(199)).Item1);
+            Console.WriteLine("总个数:{0}", a.Count());
         }
     }
 }
